dc5c4ac31706c346900d00e861f620ebc80ef745
yangfangmin <fangmin.yang@ximalaya.com>
2023-02-17 23:19:06 +0800

    添加zk

--- dubbo-project/dubbo-provider/src/main/java/com/demo/dubbo/provider/impl/UserQueryFacadeImpl.java
+++ dubbo-project/dubbo-provider/src/main/java/com/demo/dubbo/provider/impl/UserQueryFacadeImpl.java
@@ -5,6 +5,7 @@ import com.demo.dubbo.api.UserQueryFacade;
 public class UserQueryFacadeImpl implements UserQueryFacade {
     @Override
     public String queryUser(String name) {
+        System.out.println(name);
         return "hello " + name + ", my name is bob";
     }
 }
